6b5b1df829862b786e8188794b4e437cec8074d8
Hrach2003 <hrachya.hovakimyan.03@mail.ru>
2021-04-04 12:10:56 +0400

    random - grid search

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,14 +2,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.model_selection import cross_val_score
 import numpy as np
+import pandas as pd
 
 
-def classify(model, X, y):
+def classify(model, X: pd.core.api.DataFrame, y: pd.core.api.Series):
     x_train, x_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.25, random_state=42)
+        X, y, test_size=0.2, random_state=42)
     # train the model
     model.fit(x_train, y_train)
-
     accuracy = model.score(x_test, y_test) * 100
     print("Accuracy:", accuracy)
 
@@ -17,7 +17,6 @@ def classify(model, X, y):
     score = cross_val_score(model, X, y, cv=5)
     mean_CV_score = np.mean(score)*100
     print("CV Score:", mean_CV_score)
-
     y_pred = model.predict(x_test)
     print(classification_report(y_pred, y_test))
     return accuracy, mean_CV_score
