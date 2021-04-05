e8a55e1cbba7158849605dc50725ec77cfa3e273
Hrach2003 <hrachya.hovakimyan.03@mail.ru>
2021-04-05 18:15:47 +0400

    update

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.pickle_model import dump_files
+
+if __name__ == '__main__':
+    dump_files()
--- src/pickle_model.py
+++ src/pickle_model.py
@@ -1,21 +1,20 @@
-# %%
-from models import RandomForestClassifier, ExtraTreesClassifier
+from src.models.ExtraTreesClassifier import getExtraTreesModel
+from src.models.RandomForestClassifier import getRandForestModel
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import joblib
 
-# %%
-df = pd.read_csv('./data/wineQuality(cleaned).csv')
+df = pd.read_csv('src/data/wineQuality(cleaned).csv')
 X = df[df.columns.difference(['quality'])]
 y = df['quality']
 
-# %%
 x_train, x_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42)
 
-pkl_filenames = [("ExtraTreesClassifierModel.pkl", ExtraTreesClassifier.getExtraTreesModel(x_train, y_train)),
-                 ("RandomForestClassifierModel.pkl", RandomForestClassifier.getRandForestModel(x_train, y_train))]
+pkl_filenames = [("ExtraTreesClassifierModel.joblib", getExtraTreesModel(x_train, y_train)),
+                 ("RandomForestClassifierModel.joblib", getRandForestModel(x_train, y_train))]
 
-if __name__ == '__main__':
+
+def dump_files():
     for filename, model in pkl_filenames:
         joblib.dump(model, filename)
