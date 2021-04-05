46585bd7cc0c6985acfefa4a8a88b032886fbac4
Hrach2003 <hrachya.hovakimyan.03@mail.ru>
2021-04-05 10:10:40 +0400

    server predicting

--- src/pickle_model.py
+++ src/pickle_model.py
@@ -1,14 +1,15 @@
-import pickle
+# %%
 from models import RandomForestClassifier, ExtraTreesClassifier
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import joblib
 
-
-df = pd.read_csv('src/data/wineQuality(cleaned).csv')
+# %%
+df = pd.read_csv('./data/wineQuality(cleaned).csv')
 X = df[df.columns.difference(['quality'])]
 y = df['quality']
 
+# %%
 x_train, x_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42)
 
